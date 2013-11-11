990e994d54cfb85ff8b6f3e4d82da6ff1153eea5
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-11 16:06:40 -0500

    properly exiting with test_result code

--- test/main.js
+++ test/main.js
@@ -7,4 +7,6 @@ suite('third party modules');
 
 test('loads jquery from exposed jquery', function() {
   assert.equal(jquery().jquery, '1.10.2');
+  // uncomment below to see failing test
+  // assert.ok(0, 'fail');
 });
